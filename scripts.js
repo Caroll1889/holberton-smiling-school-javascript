a9667f83dc39db5386dc7f7072fc69de33a0af58
Caroll1889 <diahancaroll@hotmail.com>
2021-02-27 20:17:53 -0500

    Add: script file

---
+++ scripts.js
@@ -0,0 +1,37 @@
+function displayQuote(name, pic_url, text, title) {
+    $('#quote').append(
+        `<div class="carousel-item">
+            <div class="row text-white ml-2">
+                <div class="d-flex justify-content-center flex-wrap ">
+                    <div class="mt-4 col-sm-7 col-md-2 d-flex justify-content-center">
+                        <img src="${pic_url}" alt="" class="image" width="120" height="120">
+                    </div>
+                    <div class="mt-4 col-sm-8 col-md-7">
+                        <p class="paragraph-carousel">${text}</p>
+                        <p class="name mt-4 mb-1">${name}</p>
+                        <p class="italic">${title}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    )
+}
+
+function quote(){
+    $('.loader').show()
+    $.ajax({
+        type: 'GET',
+        url: 'https://smileschool-api.hbtn.info/quotes',
+        error: function(error){
+            alert(error)
+        },
+        success: function(response) {
+            response.forEach(({name, pic_url, text, title}) => {
+                displayQuote(name, pic_url, text, title)
+            });
+            $('.carousel .carousel-item:first').addClass('active')
+            $('.loader').hide()
+        }
+    })
+}
+quote()
