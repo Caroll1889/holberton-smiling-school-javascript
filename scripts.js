4395921e3765818c0ade62896c0af8f815d47d2a
Caroll1889 <diahancaroll@hotmail.com>
2021-03-01 21:20:47 -0500

    Add: cards function for display videos dinamicly

--- scripts.js
+++ scripts.js
@@ -17,16 +17,16 @@ function displayQuote(name, pic_url, text, title) {
     )
 }
 
-function quote(){
+function quotes() {
     $('.loader').show()
     $.ajax({
         type: 'GET',
         url: 'https://smileschool-api.hbtn.info/quotes',
-        error: function(error){
+        error: function (error) {
             alert(error)
         },
-        success: function(response) {
-            response.forEach(({name, pic_url, text, title}) => {
+        success: function (response) {
+            response.forEach(({ name, pic_url, text, title }) => {
                 displayQuote(name, pic_url, text, title)
             });
             $('.carousel .carousel-item:first').addClass('active')
@@ -34,4 +34,65 @@ function quote(){
         }
     })
 }
-quote()
+quotes()
+
+function cards(q, topic, sort) {
+    $('.loader').show()
+    $.ajax({
+        type: 'GET',
+        url: 'https://smileschool-api.hbtn.info/courses',
+        data: {
+            q,
+            topic,
+            sort,
+        },
+        error: function (error) {
+            alert(error)
+        },
+        success: function (response) {
+            if (response.courses.length === 1) {
+                let videos = document.querySelector('#videos')
+                videos.textContent = `1 video`
+            } else {
+                let videos = document.querySelector('#videos')
+                videos.textContent = `${response.courses.length} videos`
+            }
+            $('#cards').empy()
+            response.courses.forEach(function (element) {
+                $('#cards').append(
+                    `<img src="${element.thumb_url}" alt="" class="card-img-top">
+                    <div class="card-img-overlay d-flex justify-content-center mt-5">
+                        <img src="images/play.png" alt="" width="50 " height="50">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text paragraph-card">${element['sub-title']}</p>
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="${element.author_pic_url}" alt="" class="image" width="60" height="60">
+                            </div>
+                            <div class="col-6">
+                                <h3 class="ml-4 mt-3 text-center">${element.author}</h3>
+                            </div>
+                        </div>
+                        <div class="row d-flex justify-content-around">
+                            <div class="ml-3 mt-1 stars col-6">
+                            </div>
+                            <div class="col-6">
+                                <h3 class="mx-2 mt-2 text-end">${element.duration}</h3>
+                            </div>
+                        </div>
+                    </div>`)
+
+                for (let i = 1; i <= 5; i++) {
+                    if (i <= courses.star) {
+                        $('#stars').append('<img src="images/star_on.png" alt="" width="15" height="15">')
+                    } else {
+                        $('#stars').append('<img src="images/star_off.png" alt="" width="15" height="15">')
+                    }
+                }
+            })
+            $('.loader').hide()
+        }
+    })
+}
